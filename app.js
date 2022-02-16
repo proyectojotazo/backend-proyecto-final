7e0b131ca40974528886a8a57a1f58f31453687f
Josep Miquel <tommyshelby@disroot.org>
2022-02-16 23:13:40 +0100

    Get userID from JWToken

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-require("./lib/connection");
+require('./lib/connection');
 
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-const { articulosRouter, usuariosRouter } = require("./routes");
+const { articulosRouter, usuariosRouter } = require('./routes');
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/articles", articulosRouter);
-app.use("/users", usuariosRouter);
-
+app.use('/articles', articulosRouter);
+app.use('/users', usuariosRouter);
 
 module.exports = app;
--- routes/articulos.js
+++ routes/articulos.js
@@ -1,15 +1,15 @@
-const express = require("express");
+const express = require('express');
 const articulosRouter = express.Router();
-const { Articulo } = require("../models");
-const { jwtAuth } = require("../middlewares");
+const { Articulo } = require('../models');
+const { jwtAuth } = require('../middlewares');
+const getUserFromJwt = require('../utils/getUserFromJwt');
 
 /* GET home page. */
-articulosRouter.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+articulosRouter.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
-
-articulosRouter.post("/", jwtAuth, async (req, res, next) => {
+articulosRouter.post('/', jwtAuth, async (req, res, next) => {
   const {
     titulo,
     archivoDestacado,
@@ -18,9 +18,13 @@ articulosRouter.post("/", jwtAuth, async (req, res, next) => {
     // fechaPublicacion,
     estado,
     categorias,
-    usuario,
-    comentarios } = req.body;
+    comentarios,
+  } = req.body;
 
+  const jwtToken =
+    req.get('Authorization') || req.query.token || req.body.token;
+
+  const usuario = getUserFromJwt(jwtToken);
 
   try {
     const nuevoArticulo = new Articulo({
@@ -32,16 +36,16 @@ articulosRouter.post("/", jwtAuth, async (req, res, next) => {
       estado,
       categorias,
       usuario,
-      comentarios
+      comentarios,
     });
     await nuevoArticulo.save();
-    res.json({ msj: "articulo insertado de forma satifactoria", id: nuevoArticulo._id });
+    res.json({
+      msj: 'articulo insertado de forma satifactoria',
+      id: nuevoArticulo._id,
+    });
   } catch (error) {
-    res.json(error)
+    res.json(error);
   }
 });
-
-
-
 
 module.exports = articulosRouter;
