4473e8b305e2670becc1ea34c0ee1af374cc7821
Javier <javigh87@gmail.com>
2022-02-16 16:20:03 +0100

    Middleware jwtAuth

---
+++ middlewares/jwtAuth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+const jwtAuth = async (req, res, next) => {
+  const jwtToken =
+    req.get("Authorization") || req.query.token || req.body.token;
+
+  // Comprobamos existencia de Token
+  if (!jwtToken) {
+    const error = new Error("No se ha proporcionado token");
+    error.status = 401;
+    return res.status(error.status).json(error.message);
+  }
+
+  try {
+    await jwt.verify(jwtToken, process.env.JWT_SECRET);
+    next();
+    return;
+  } catch (error) {
+    const err = new Error(error.message);
+    err.status = 401;
+    return res.status(error.status).json(error.message);
+  }
+};
+
+module.exports = jwtAuth;
