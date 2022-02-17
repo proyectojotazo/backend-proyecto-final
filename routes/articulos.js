ed2e0d07d7b0ef148541476bb10e3685a74abfe4
Javier <javigh87@gmail.com>
2022-02-17 10:16:06 +0100

    Añadido al crear articulo que el usuario tambien tenga la id del articulo

--- routes/articulos.js
+++ routes/articulos.js
@@ -1,30 +1,32 @@
-const express = require('express');
+const express = require("express");
 const articulosRouter = express.Router();
-const { Articulo } = require('../models');
-const { jwtAuth } = require('../middlewares');
-const getUserFromJwt = require('../utils/getUserFromJwt');
+const { Articulo, Usuario } = require("../models");
+const { jwtAuth } = require("../middlewares");
+const getUserFromJwt = require("../utils/getUserFromJwt");
 
 /* GET home page. */
-articulosRouter.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+articulosRouter.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-articulosRouter.post('/', jwtAuth, async (req, res, next) => {
+articulosRouter.post("/", jwtAuth, async (req, res, next) => {
   const {
     titulo,
     archivoDestacado,
     textoIntroductorio,
     contenido,
-    // fechaPublicacion,
     estado,
     categorias,
     comentarios,
   } = req.body;
 
   const jwtToken =
-    req.get('Authorization') || req.query.token || req.body.token;
+    req.get("Authorization") || req.query.token || req.body.token;
 
-  const usuario = getUserFromJwt(jwtToken);
+  const usuarioId = getUserFromJwt(jwtToken);
+
+  // Obtenemos al usuario para actualizarlo con el nuevo post cread
+  const usuario = await Usuario.findById(usuarioId);
 
   try {
     const nuevoArticulo = new Articulo({
@@ -32,19 +34,29 @@ articulosRouter.post('/', jwtAuth, async (req, res, next) => {
       archivoDestacado,
       textoIntroductorio,
       contenido,
-      // fechaPublicacion: Date.now(),
       estado,
       categorias,
-      usuario,
+      usuario: usuarioId,
       comentarios,
     });
+
     await nuevoArticulo.save();
+
+    // Actualizamos los articulos del usuario haciendo uso del operador SPREAD
+    await Usuario.findByIdAndUpdate(usuarioId, {
+      articulos: {
+        creados: [...usuario.articulos.creados, nuevoArticulo._id],
+        favoritos: [...usuario.articulos.favoritos],
+      },
+    });
+
     res.json({
-      msj: 'articulo insertado de forma satifactoria',
+      msj: "articulo insertado de forma satifactoria",
       id: nuevoArticulo._id,
     });
+    
   } catch (error) {
-    res.json(error);
+    res.status(400).json(error);
   }
 });
 
