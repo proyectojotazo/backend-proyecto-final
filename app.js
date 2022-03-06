e2fa7329eb8d98fd27cb357974be9bcbc4988dbd
MiwelR <rrriosss@gmail.com>
2022-03-07 00:12:54 +0100

    CORS añadido

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 const { errorHandler } = require("./middlewares");
 const swaggerUi = require("swagger-ui-express");
 const swaggerConfig = require("./swagger/swaggerConfig");
@@ -22,6 +23,7 @@ const {
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
